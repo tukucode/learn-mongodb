ad51c0599cd45d5ed76a08c6d2bc09fbbeee2644
tomimandalap <tommymandalaputra@gmail.com>
2024-01-04 20:20:16 +0700

    implementation seeding in index.js test

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import cors from "cors";
 import roleRoutes from "./src/routes/role.js";
 import userRoutes from "./src/routes/user.js";
 
+// SEEDING
+import seed from "./src/utils/seed.js";
+
 const app = express();
 
 // setup CORS
@@ -12,6 +15,9 @@ app.use(cors());
 // setup body parser
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ limit: "1mb", extended: true }));
+
+// SEEDING
+await seed();
 
 // ROUTES
 app.use("/api/v1", roleRoutes);
